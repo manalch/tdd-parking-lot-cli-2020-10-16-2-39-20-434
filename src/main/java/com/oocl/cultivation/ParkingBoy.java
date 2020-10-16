93787059fe4ca4ca167516c447098a74ce565542
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-16 14:27:49 +0800

    [TDD-ParkingLot] [Joseph] test: add a test for parking boy to fetch a car and return the car

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -4,4 +4,8 @@ public class ParkingBoy {
     public ParkingTicket park(Car car) {
         return new ParkingTicket();
     }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return null;
+    }
 }
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -1,20 +1,43 @@
 package com.oocl.cultivation;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class ParkingBoyTest {
+    private ParkingBoy parkingBoy;
+    private Car car;
+    private ParkingTicket parkingTicket;
+
+    @BeforeEach
+    void setUp() {
+        parkingBoy = new ParkingBoy();
+        car = new Car();
+        parkingTicket = new ParkingTicket();
+    }
+
     @Test
     void should_return_a_parking_ticket_when_parking_boy_park_the_car_into_parking_lot_given_a_car() {
         //given
-        ParkingBoy parkingBoy = new ParkingBoy();
-        Car car = new Car();
 
         //when
         ParkingTicket parkingTicket = parkingBoy.park(car);
 
         //then
         assertNotNull(parkingTicket);
+    }
+
+    @Test
+    void should_return_fetched_car_when_parking_boy_fetch_the_car_given_a_parking_ticket() {
+        //given
+        parkingTicket = parkingBoy.park(car);
+
+        //when
+        Car fetchedCar = parkingBoy.fetch(parkingTicket);
+
+        //then
+        assertSame(car, fetchedCar);
     }
 }
