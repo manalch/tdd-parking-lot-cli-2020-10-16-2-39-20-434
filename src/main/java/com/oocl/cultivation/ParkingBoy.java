105dd9844eb1fe4b3973cd724e71a990c597896f
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-17 23:21:59 +0800

    [TDD-ParkingLot] [Joseph] feature: implement feature for parking boy when park another car given parking lot is already full

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,22 +1,25 @@
 package com.oocl.cultivation;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class ParkingBoy {
     private Map<ParkingTicket, Car> parkingTicketCarMap = new HashMap<>();
     private ParkingLot parkingLot;
+    private List<ParkingLot> parkingLots = new ArrayList<>();
 
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
 
     public ParkingTicket park(Car car) {
+        if (hasNoAvailableParkingLot()){
+            throw new RuntimeException("Not enough position");
+        }
         ParkingTicket parkingTicket = new ParkingTicket();
         parkingTicketCarMap.put(parkingTicket, car);
         parkingLot.parkCar(car);
-        return hasAvailableSlots() ? parkingTicket : null;
+
+        return parkingTicket;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
@@ -32,8 +35,8 @@ public class ParkingBoy {
         return car;
     }
 
-    private boolean hasAvailableSlots() {
-        return parkingLot.getParkedCars().size() <= parkingLot.getCapacity();
+    private boolean hasNoAvailableParkingLot() {
+        return parkingLot.getParkedCars().size() >= parkingLot.getCapacity();
     }
 
     private boolean isUnrecognizedParkingTicket(ParkingTicket parkingTicket) {
