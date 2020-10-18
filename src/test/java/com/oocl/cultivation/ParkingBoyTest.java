459f6415e036d006a76e31d89bde846113fae4eb
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-18 09:47:59 +0800

    [TDD-ParkingLot] [Joseph] test: add a tests for parking boy to handle multiple parking lots

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -3,6 +3,9 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingBoyTest {
@@ -93,5 +96,89 @@ class ParkingBoyTest {
         assertNotNull(parkingTicket1);
         Throwable runtimeException = assertThrows(RuntimeException.class, () -> parkingBoy.park(new Car()));
         assertEquals(runtimeException.getMessage(), "Not enough position");
+    }
+
+    @Test
+    void should_park_to_lot_1_when_parking_boy_park_the_car_given_a_car_and_parking_lot1_is_not_yet_full() {
+        ParkingLot parkingLot1 = new ParkingLot();
+        parkingLot1.setCapacity(10);
+        ParkingLot parkingLot2 = new ParkingLot();
+        parkingLot2.setCapacity(10);
+
+        List<ParkingLot> assignedParkingLots = Arrays.asList(parkingLot1, parkingLot2);
+        parkingBoy = new ParkingBoy(assignedParkingLots);
+
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+        parkingLot1 = parkingBoy.getParkingLots().get(0);
+        parkingLot2 = parkingBoy.getParkingLots().get(1);
+
+        assertNotNull(parkingTicket);
+        assertTrue(parkingLot1.getParkedCars().contains(car));
+        assertFalse(parkingLot2.getParkedCars().contains(car));
+    }
+
+    @Test
+    void should_park_to_lot_2_when_parking_boy_park_the_car_given_a_car_and_parking_lot1_is_full_and_parking_lot2_is_not_full() {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        parkingLot1.setCapacity(1);
+        ParkingLot parkingLot2 = new ParkingLot();
+        parkingLot2.setCapacity(5);
+        List<ParkingLot> assignedParkingLots = Arrays.asList(parkingLot1, parkingLot2);
+        parkingBoy = new ParkingBoy(assignedParkingLots);
+
+        ParkingTicket parkingTicket1 = parkingBoy.park(car1);
+        ParkingTicket parkingTicket2 = parkingBoy.park(car2);
+        ParkingTicket parkingTicket3 = parkingBoy.park(car3);
+        parkingLot1 = parkingBoy.getParkingLots().get(0);
+        parkingLot2 = parkingBoy.getParkingLots().get(1);
+
+        assertNotNull(parkingTicket1);
+        assertNotNull(parkingTicket2);
+        assertNotNull(parkingTicket3);
+
+        assertFalse(parkingLot1.getParkedCars().contains(car3));
+        assertFalse(parkingLot1.getParkedCars().contains(car2));
+        assertFalse(parkingLot2.getParkedCars().contains(car1));
+
+        assertTrue(parkingLot1.getParkedCars().contains(car1));
+        assertTrue(parkingLot2.getParkedCars().contains(car2));
+        assertTrue(parkingLot2.getParkedCars().contains(car3));
+    }
+
+    @Test
+    void should_not_park_when_parking_boy_park_the_car_given_a_car_and_parking_lot1_and_parking_lot2_is_full() {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        parkingLot1.setCapacity(1);
+        ParkingLot parkingLot2 = new ParkingLot();
+        parkingLot2.setCapacity(1);
+        List<ParkingLot> assignedParkingLots = Arrays.asList(parkingLot1, parkingLot2);
+        parkingBoy = new ParkingBoy(assignedParkingLots);
+
+        ParkingTicket parkingTicket1 = parkingBoy.park(car1);
+        assertNotNull(parkingTicket1);
+
+        ParkingTicket parkingTicket2 = parkingBoy.park(car2);
+        assertNotNull(parkingTicket2);
+
+        assertFalse(parkingLot1.getParkedCars().contains(car2));
+        assertFalse(parkingLot2.getParkedCars().contains(car1));
+
+        assertTrue(parkingLot1.getParkedCars().contains(car1));
+        assertTrue(parkingLot2.getParkedCars().contains(car2));
+
+        Throwable runtimeException = assertThrows(RuntimeException.class, () -> parkingBoy.park(car3));
+        assertEquals(runtimeException.getMessage(), "Not enough position");
+
+        parkingLot1 = parkingBoy.getParkingLots().get(0);
+        parkingLot2 = parkingBoy.getParkingLots().get(1);
+
+        assertFalse(parkingLot1.getParkedCars().contains(car3));
+        assertFalse(parkingLot2.getParkedCars().contains(car3));
     }
 }
